de2af77546833bafc333942c223c867a2bee5f6a
khanfou <44687848+khanfou@users.noreply.github.com>
2023-07-18 14:14:12 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from datasets import load_dataset
+
+dataset_dict = load_dataset('HUPD/hupd',streaming = true, train_filing_start_date='2016-01-01',train_filing_end_date='2016-01-31',val_filing_start_date='2017-01-01',val_filing_end_date='2017-01-31')
+
+df = pd.DataFrame.from_dict(dataset_dict["train"])
+
+# Create a DataFrame object from list
+df = pd.DataFrame(df,columns =['patent_number','decision', 'abstract', 'claims','filing_date'])
+st.dataframe(df)
